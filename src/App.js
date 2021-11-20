dd789607f313b9efd40b89043a1ed714ea31ff85
Louis D. Guillermo <louisd.guillermo@Louiss-MacBook-Pro-2.local>
2021-11-20 17:02:46 -0500

    carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import Carousel from './components/Carousel';
 import Header from './components/Header'
 import Main from './components/Main'
 import Footer from './components/Footer'
+import CarouselData from './components/CarouselData';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <h2>Special Offers</h2>
+      <Carousel carouselImgs={CarouselData}/>
       <Main />
       <Footer />
     </div>
